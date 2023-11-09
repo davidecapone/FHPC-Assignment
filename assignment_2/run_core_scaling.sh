affa5f1d8f3f8fd49ed01c9e9fcf306152fe004f
Davide Capone <dcapone@login01.hpc.rd.areasciencepark.it>
2023-11-09 19:30:32 +0000

    size scaling epyc tests

---
+++ assignment_2/run_core_scaling.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#SBATCH --no-requeue
+#SBATCH --partition=EPYC
+#SBATCH --cpus-per-task=64
+#SBATCH --job-name=core_scaling
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH --time=02:00:00
+#SBATCH --output=/u/dssc/dcapone/fast/FHPC-Assignment/assignment_2/slurm.out/core_scaling%j.out
+
+# usage: sbatch run_core_scaling.sh <spread|close> <EPYC|THIN>
+POLICY=$1
+NODE=$2
+
+# libraries
+module load mkl
+module load openBLAS/0.3.23-omp
+
+SCRIPT_DIR=/u/dssc/dcapone/fast/FHPC-Assignment/assignment_2/
+cd $SCRIPT_DIR
+
+## !! consider the possibility or removing the make clean and make cpu:
+#make clean
+#make cpu
+
+export OMP_PROC_BIND=$POLICY
+
+echo "n_cores,size,gflops" > "./results/core_scaling/core_scaling_${NODE}_mkl_float_${POLICY}.csv"
+echo "n_cores,size,gflops" > "./results/core_scaling/core_scaling_${NODE}_oblas_float_${POLICY}.csv"
+echo "n_cores,size,gflops" > "./results/core_scaling/core_scaling_${NODE}_mkl_double_${POLICY}.csv"
+echo "n_cores,size,gflops" > "./results/core_scaling/core_scaling_${NODE}_oblas_double_${POLICY}.csv"
+
+# used cores goes from 1 to max_cores
+max_cores=64
+
+for i in {1..$max_cores}
+do	
+	# set number cores
+	set OMP_NUM_THREADS=$i
+
+	cd $SCRIPT_DIR
+	# fixing the number of cores, the test is executed 10 times
+	for _ in {1..10}
+	do
+		# mkl float
+		echo "${i}" >> ./results/core_scaling/core_scaling_${NODE}_mkl_float_${POLICY}.csv
+		./gemm_mkl_float.x 15000 15000 15000 >> ./results/core_scaling/core_scaling_${NODE}_mkl_float_${POLICY}.csv
+
+		# openblas float
+		echo|set /p=$i, >> ./results/core_scaling/core_scaling_${NODE}_oblas_float_${POLICY}.csv
+        ./gemm_oblas_float.x 15000 15000 15000 >> ./results/core_scaling/core_scaling_${NODE}_oblas_float_${POLICY}.csv
+
+		# mkl double
+		echo|set /p=$i, >> ./results/core_scaling/core_scaling_${NODE}_mkl_float_${POLICY}.csv
+		./gemm_mkl_double.x 15000 15000 15000 >> ./results/core_scaling/core_scaling_${NODE}_mkl_double_${POLICY}.csv
+
+		# openblas double
+		echo|set /p=$i, >> ./results/core_scaling/core_scaling_${NODE}_oblas_float_${POLICY}.csv
+        ./gemm_oblas_double.x 15000 15000 15000 >> ./results/core_scaling/core_scaling_${NODE}_oblas_double_${POLICY}.csv
+        done
+done
+END
--- assignment_2/run_size_scaling.sh
+++ assignment_2/run_size_scaling.sh
@@ -1,50 +1,57 @@
 #!/bin/bash
 #SBATCH --no-requeue
 #SBATCH --partition=EPYC
+#SBATCH --cpus-per-task=64
+#SBATCH --job-name=size_scaling
 #SBATCH --nodes=1
 #SBATCH --exclusive
 #SBATCH --time=02:00:00
-#SBATCH --output=%j.out
-#SBATCH --cpus-per-task=64
+#SBATCH --output=/u/dssc/dcapone/fast/FHPC-Assignment/assignment_2/slurm.out/size_scaling%j.out
 
-# $1 : "spread" or "close"
+# --> 64 per EPYC node, 12 per THIN node
 
-# load libraries
+# usage: sbatch run_size_scaling.sh <spread|close> <EPYC|THIN>
+POLICY=$1
+NODE=$2
+
+# libraries
 module load mkl
 module load openBLAS/0.3.23-omp
 
-# parent path: assignment_2
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path"
-make clean
-make cpu
+SCRIPT_DIR=/u/dssc/dcapone/fast/FHPC-Assignment/assignment_2/
+cd $SCRIPT_DIR
 
-# SPREAD = spread threads over cores
-export OMP_PROC_BIND=$1
+## !! consider the possibility of removing the make clean and make cpu:
+#make clean
+#make cpu
 
+export OMP_PROC_BIND=$POLICY
 
-echo "size,gflops" > ./results/size_scaling_EPYC_mkl_float_$1.csv
-echo "size,gflops" > ./results/size_scaling_EPYC_oblas_float_$1.csv
-echo "size,gflops" > ./results/size_scaling_EPYC_mkl_double_$1.csv
-echo "size,gflops" > ./results/size_scaling_EPYC_oblas_double_$1.csv    
+echo "size,gflops" > "./results/size_scaling/size_scaling_${NODE}_mkl_float_${POLICY}.csv"
+echo "size,gflops" > "./results/size_scaling/size_scaling_${NODE}_oblas_float_${POLICY}.csv"
+echo "size,gflops" > "./results/size_scaling/size_scaling_${NODE}_mkl_double_${POLICY}.csv"
+echo "size,gflops" > "./results/size_scaling/size_scaling_${NODE}_oblas_double_${POLICY}.csv"  
 
 # size of the matrix goes from 2000 to 20000, step of 1000
+echo "Starting size scaling test on node $NODE using $POLICY policy\n"
 for i in {0..18}
 do  let size=$((2000+1000*$i))
     # fixing the size of the matrix, the test is executed 10 times
     for _ in {1..10}
     do
-        echo $size
+        echo "size: $size"
+        cd $SCRIPT_DIR
         # mkl float
-        ./gemm_mkl_float.x $size $size $size >> ./results/size_scaling_EPYC_mkl_float_$1.csv
-
+        ./gemm_mkl_float.x $size $size $size >> "./results/size_scaling/size_scaling_${NODE}_mkl_float_${POLICY}.csv"
+        echo "mkl float done"
         # openblas float
-        ./gemm_oblas_float.x $size $size $size >> ./results/size_scaling_EPYC_oblas_float_$1.csv
-
+        ./gemm_oblas_float.x $size $size $size >> "./results/size_scaling/size_scaling_${NODE}_oblas_float_${POLICY}.csv"
+        echo "openblas float done"
         # mkl double
-	    ./gemm_mkl_double.x $size $size $size >> ./results/size_scaling_EPYC_mkl_double_$1.csv
-
+	    ./gemm_mkl_double.x $size $size $size >> "./results/size_scaling/size_scaling_${NODE}_mkl_double_${POLICY}.csv"
+        echo "mkl double done"
         # openblas double
-        ./gemm_oblas_double.x $size $size $size >> ./results/size_scaling_EPYC_oblas_double_$1.csv
+        ./gemm_oblas_double.x $size $size $size >> "./results/size_scaling/size_scaling_${NODE}_oblas_double_${POLICY}.csv"
+        echo "openblas double done"
     done
 done
