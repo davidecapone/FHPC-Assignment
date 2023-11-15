4cb40866c8e428c89653ab584e0c28f40ce0dee8
Davide Capone <dcapone@login01.hpc.rd.areasciencepark.it>
2023-11-15 13:03:28 +0000

    core scaling results

--- assignment_2/README.md
+++ assignment_2/README.md
@@ -1,3 +1,5 @@
 # Assignment 2
 
-The assignment 2.
+
+
+
--- assignment_2/run_core_scaling.sh
+++ assignment_2/run_core_scaling.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 #SBATCH --no-requeue
-#SBATCH --partition=EPYC
-#SBATCH --cpus-per-task=64
+#SBATCH --partition=THIN
 #SBATCH --job-name=core_scaling
 #SBATCH --nodes=1
 #SBATCH --exclusive
 #SBATCH --time=02:00:00
 #SBATCH --output=/u/dssc/dcapone/fast/FHPC-Assignment/assignment_2/slurm.out/core_scaling%j.out
-
-# --> 64 per EPYC node, 12 per THIN node
 
 # usage: sbatch run_core_scaling.sh <spread|close> <EPYC|THIN>
 POLICY=$1
@@ -28,9 +25,9 @@ cd $SCRIPT_DIR
 export OMP_PROC_BIND=$POLICY
 
 echo "n_cores,size,gflops" > "./results/core_scaling/core_scaling_${NODE}_mkl_float_${POLICY}.csv"
-echo "n_cores,size,gflops" > "./results/core_scaling/core_scaling_${NODE}_oblas_float_${POLICY}.csv"
-echo "n_cores,size,gflops" > "./results/core_scaling/core_scaling_${NODE}_mkl_double_${POLICY}.csv"
-echo "n_cores,size,gflops" > "./results/core_scaling/core_scaling_${NODE}_oblas_double_${POLICY}.csv"
+#echo "n_cores,size,gflops" > "./results/core_scaling/core_scaling_${NODE}_oblas_float_${POLICY}.csv"
+#echo "n_cores,size,gflops" > "./results/core_scaling/core_scaling_${NODE}_mkl_double_${POLICY}.csv"
+#echo "n_cores,size,gflops" > "./results/core_scaling/core_scaling_${NODE}_oblas_double_${POLICY}.csv"
 
 # used cores goes from 1 to max_cores
 max_cores=64
@@ -48,28 +45,38 @@ for ((i=1; i<=$max_cores; i++)) do
 		# - we save the output 
 		# - we parse the output and save it in the csv file (n_cores, size, gflops)
 		# this process foreach library:
-		IFS=',' read -ra ADDR <<< "$(./gemm_mkl_float.x 15000 15000 15000)"
+
+		#: << 'END'
+		IFS=',' read -ra ADDR <<< "$(./gemm_mkl_float.x 10000 10000 10000)"
 		size=${ADDR[0]}
 		gflops=${ADDR[1]}
 		echo "$i,$size,$gflops" >> ./results/core_scaling/core_scaling_${NODE}_mkl_float_${POLICY}.csv
 		echo "mkl float done"
+#END
 
-		IFS=',' read -ra ADDR <<< "$(./gemm_oblas_float.x 15000 15000 15000)"
+		: << 'END'
+		IFS=',' read -ra ADDR <<< "$(./gemm_oblas_float.x 10000 10000 10000)"
 		size=${ADDR[0]}
 		gflops=${ADDR[1]}
 		echo "$i,$size,$gflops" >> ./results/core_scaling/core_scaling_${NODE}_oblas_float_${POLICY}.csv
 		echo "openblas float done"
+END
 
-		IFS=',' read -ra ADDR <<< "$(./gemm_mkl_double.x 15000 15000 15000)"
+
+		: << 'END'
+		IFS=',' read -ra ADDR <<< "$(./gemm_mkl_double.x 10000 10000 10000)"
 		size=${ADDR[0]}
 		gflops=${ADDR[1]}
 		echo "$i,$size,$gflops" >> ./results/core_scaling/core_scaling_${NODE}_mkl_double_${POLICY}.csv
 		echo "mkl double done"
-
-		IFS=',' read -ra ADDR <<< "$(./gemm_oblas_double.x 15000 15000 15000)"
+END
+	
+		: << 'END'
+		IFS=',' read -ra ADDR <<< "$(./gemm_oblas_double.x 10000 10000 10000)"
 		size=${ADDR[0]}
 		gflops=${ADDR[1]}
 		echo "$i,$size,$gflops" >> ./results/core_scaling/core_scaling_${NODE}_oblas_double_${POLICY}.csv
 		echo "openblas double done"
+END
 	done
 done
