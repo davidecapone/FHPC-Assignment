c91056e7c54cbb2736f9af2c93e411da30ba0d5d
Davide Capone <dcapone@login01.hpc.rd.areasciencepark.it>
2023-11-27 17:51:35 +0000

    main file added

---
+++ assignment_1/main.c
@@ -0,0 +1,63 @@
+/*
+This file C file is based on the code given by the professor, original code:
+    https://github.com/Foundations-of-HPC/Foundations_of_HPC_2022/blob/main/Assignment/exercise1/get_args.c
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <getopt.h>
+
+#define INIT 1
+#define RUN  2
+#define K_DFLT 100
+#define ORDERED 0
+#define STATIC  1
+
+char fname_deflt[] = "game_of_life.pgm";
+int   action = 0;
+int   k      = K_DFLT;
+int   e      = ORDERED;
+int   n      = 10000;
+int   s      = 1;
+char *fname  = NULL;
+
+int main ( int argc, char **argv ) {
+  int action = 0;
+  char *optstring = "irk:e:f:n:s:";
+  int c;
+
+  while ((c = getopt(argc, argv, optstring)) != -1) {
+
+    switch(c) {
+      case 'i':
+        action = INIT; 
+        break;
+      case 'r':
+        action = RUN; 
+        break;
+      case 'k':
+        k = atoi(optarg); 
+        break;
+      case 'e':
+        e = atoi(optarg); 
+        break;
+      case 'f':
+        fname = (char*)malloc( sizeof(optarg)+1 );
+        sprintf(fname, "%s", optarg );
+        break;
+      case 'n':
+        n = atoi(optarg); 
+        break;
+      case 's':
+        s = atoi(optarg); 
+        break;
+      default :
+        printf("argument -%c not known\n", c ); 
+        break;
+    }
+  }
+
+  if ( fname != NULL ) free ( fname );
+  return 0;
+}
