728b3a075b51feec598f37a64377f69163f6d878
5JDR <dellarovere.sandro@gmail.com>
2023-12-04 10:17:13 +0000

    test on bigger game size

--- assignment_1/slurm-scripts/run.sh
+++ assignment_1/slurm-scripts/run.sh
@@ -9,7 +9,7 @@
 #SBATCH --output=/u/dssc/drsandro/fast/FHPC-Assignment/assignment_1/slurm.out/correctness_test_%j.out
 
 # number of MPI processes
-n_MPI_proc=10
+n_MPI_proc=25
 
 module purge
 #module load architecture/AMD
@@ -24,8 +24,8 @@ export OMP_PLACES=cores
 export OMP_PROC_BIND=close
 export OMP_NUM_THREADS=64
 
-k=5           # playground size
-e=0           # evolution type, 0 ordered, 1 static
+k=100           # playground size
+e=1           # evolution type, 0 ordered, 1 static
 n=3           # number of steps to be calculated
 s=1           # every how many steps take a snapshot (0 means only at the end)
 
