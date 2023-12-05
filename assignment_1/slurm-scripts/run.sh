455aa043572c0360c0ee785321dbfc6fd63064ca
5JDR <dellarovere.sandro@gmail.com>
2023-12-05 15:40:48 +0000

    corrected run for THIN

--- assignment_1/slurm-scripts/run.sh
+++ assignment_1/slurm-scripts/run.sh
@@ -2,14 +2,14 @@
 #SBATCH --no-requeue
 #SBATCH --job-name="conw_test"
 #SBATCH --get-user-env
-#SBATCH --partition=EPYC
+#SBATCH --partition=THIN
 #SBATCH --nodes=1
 #SBATCH --exclusive
 #SBATCH --time=02:00:00
 #SBATCH --output=/u/dssc/drsandro/fast/FHPC-Assignment/assignment_1/slurm.out/correctness_test_%j.out
 
 # number of MPI processes
-n_MPI_proc=25
+n_MPI_proc=5
 
 module purge
 #module load architecture/AMD
@@ -22,7 +22,7 @@ make
 # set the environment variables
 export OMP_PLACES=cores
 export OMP_PROC_BIND=close
-export OMP_NUM_THREADS=64
+export OMP_NUM_THREADS=12
 
 k=100           # playground size
 e=1           # evolution type, 0 ordered, 1 static
