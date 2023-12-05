571201ff71a1786573191f4b0e0291a39f238aae
5JDR <dellarovere.sandro@gmail.com>
2023-12-05 13:17:15 +0000

    added omp scalability with size 20000

--- assignment_1/slurm-scripts/omp_scalability.sh
+++ assignment_1/slurm-scripts/omp_scalability.sh
@@ -18,20 +18,39 @@ export OMP_PLACES=cores
 export OMP_PROC_BIND=close
 
 k=10000
-e=1
+e=0
 n=10
 
 mpirun -np 2 ./main.x -i -k $k
 
-echo size,OMP-threads,time > results/omp_static_$k.csv
+echo size,OMP-threads,time > results/omp_ordered.csv
 
 for i in {1..64}
 do
 	export OMP_NUM_THREADS=$i
 	for j in {1..10}
 	do
-		echo -n $k,$i >> results/omp_static_$k.csv
-	        mpirun -np 1 --map-by node --bind-to socket ./main.x -r -n $n -e $e >> results/omp_static_$k.csv
+		echo -n $k,$i >> results/omp_ordered.csv
+	    mpirun -np 1 --map-by node --bind-to socket ./main.x -r -n $n -e $e >> results/omp_ordered.csv
+	done
+done
+
+make clean
+make clean_images
+
+k=20000
+
+mpirun -np 2 ./main.x -i -k $k
+
+echo size,OMP-threads,time > results/omp_ordered.csv
+
+for i in {1..64}
+do
+	export OMP_NUM_THREADS=$i
+	for j in {1..10}
+	do
+		echo -n $k,$i >> results/omp_ordered.csv
+	    mpirun -np 1 --map-by node --bind-to socket ./main.x -r -n $n -e $e >> results/omp_ordered.csv
 	done
 done
 
