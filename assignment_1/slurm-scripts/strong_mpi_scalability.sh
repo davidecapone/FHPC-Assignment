cb66097cf7e690a80c485b3a0ccbdc5097f384b5
5JDR <dellarovere.sandro@gmail.com>
2023-12-09 09:19:07 +0000

    corrected script

--- assignment_1/slurm-scripts/strong_mpi_scalability.sh
+++ assignment_1/slurm-scripts/strong_mpi_scalability.sh
@@ -19,18 +19,18 @@ export OMP_PROC_BIND=close
 export OMP_NUM_THREADS=1
 
 k=10000     # size of the matrix (10000, 20000)
-e=0         # evolution type (0, 1)
+e=1         # evolution type (0, 1)
 
 mpirun -np 2 ./main.x -i -k $k
 
-echo size,cores,time > results/strong_MPI_ordered_$k.csv # remember to change the name of the file for each test
+echo size,cores,time > results/strong_MPI_static_$k.csv # remember to change the name of the file for each test
 
 for i in {1..128}
 do
     for j in {1..10}
     do
-        echo -n $k,$i >> results/strong_MPI_ordered_$k.csv
-	    mpirun -np $i --map-by core ./main.x -r -n 10 -e $e -t >> results/strong_MPI_ordered_$k.csv
+        echo -n $k,$i >> results/strong_MPI_static_$k.csv
+	    mpirun -np $i --map-by core ./main.x -r -n 10 -e $e >> results/strong_MPI_static_$k.csv
     done
 done
 
